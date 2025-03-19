e3b2e870c95705781053344db4c6ed12992e33a2
jjenus <biz.jenus@gmail.com>
2025-03-19 23:55:34 +0100

    vercel servless

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	nitro: {
+		preset: "vercel", // Ensure this is set correctly
+	},
 	modules: ["nuxt-swiper", "@nuxt/image"],
 	devtools: { enabled: false },
 	ssr: true,
